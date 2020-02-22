6ccbec260654ce351c4b5b8d74033f1ed163318a
SGuard27 <Guards27@hotmail.com>
2020-02-22 01:40:58 +0000

    test

--- Server/routes/dbHandler.js
+++ Server/routes/dbHandler.js
@@ -3,6 +3,13 @@
 
 
 
+var genRandomString = function(length){
+    return crypto.randomBytes(Math.ceil(length/2))
+            .toString('hex') /** convert to hexadecimal format */
+            .slice(0,length);   /** return required number of characters */
+};
+
+
 function addUser(username, password, salt){
 	//Do stuff
 }
