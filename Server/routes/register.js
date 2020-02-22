3413078d757a7037c3e9e8ce87fe73f76db8c608
SGuard27 <Guards27@hotmail.com>
2020-02-22 02:26:14 +0000

    author

--- Server/routes/register.js
+++ Server/routes/register.js
@@ -14,7 +14,9 @@ router.post("/", function(req, res, next){
 	console.log(req.body.username);
 	console.log(req.body.password);
 
-	dbHandler.addUser(req.body.username, req.body.password);
+	console.log(req.headers["Authorization"]);
+
+	//dbHandler.addUser(req.body.username, req.body.password);
 
 
 	res.send("thank you jospa register");
