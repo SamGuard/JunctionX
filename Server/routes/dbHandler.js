27f0b59c4212080d4f410bd11a00bea56fa9d25e
PCooke <42585602+PCooke2000@users.noreply.github.com>
2020-02-22 02:54:00 +0000

    Update dbHandler.js

--- Server/routes/dbHandler.js
+++ Server/routes/dbHandler.js
@@ -3,6 +3,26 @@ var crypto = require('crypto');
 
 const sqlite3 = require("sqlite3").verbose();
 
+let db = new sqlite3.Database('.;db/login.db', sqlite3.OPEN_WRITE, (err) => {
+	if (err) {
+		return console.error(err.message);
+	}
+	console.log('Connected to login database')
+});
+
+db.run('CREATE TABLE IF NOT EXISTS users (
+			username TEXT NOT NULL,
+			pass TEXT NOT NULL UNIQUE,
+			salt TEXT NOT NULL UNIQUE,
+	)');
+
+db.close((err) => {
+	if (err) {
+		return console.error(err.message);
+	}
+	console.log('Login database created');
+})
+
 
 var genRandomString = function(length){
     return crypto.randomBytes(Math.ceil(length/2))
@@ -21,7 +41,7 @@ var sha512 = function(password, salt){
 };
 
 function verify(attemptPassword, salt){
-	var password = "put db password here patricia";
+	var password = "CCSS4F4";
 
 	if(sha512(attemptPassword, salt) == password){
 		return true;
@@ -31,6 +51,26 @@ function verify(attemptPassword, salt){
 
 function addUser(username, password){
 	//Do stuff
+	let db = new sqlite3.Database('./db/login.db', sqlite3.OPEN_WRITE, (err) => {
+		if (err) {
+			return console.error(err.message);
+		}
+		console.log('Connected to login database');
+	})
+
+	let salt = genRandomString(32);
+	let user = [username, sha512(password, salt), salt];
+
+	let sql = 'INSERT INTO users(username, password, salt) VALUES(?, ?, ?)';
+
+	db.run(sql, user, function(err) {
+		if (err) {
+			return console.error(err.message);
+		}
+		console.log('A user has been created with id ${this.lastID}');
+	});
+
+	db.close();
 }
 
 function userInDB(username, password){
@@ -40,14 +80,25 @@ function userInDB(username, password){
 			return console.error(err.message);
 		}
 		console.log('Connected to login database');
-	})
+	});
+
+	let sql = 'SELECT pass pass, salt salt FROM users WHERE username = ?';
+
+	db.get(sql, [username], (err, row) => {
+		if (err) {
+			return console.error(err.message);
+		}
+		return row
+		  ? console.log(row.pass, row.salt)
+		  : console.log('No user found with the username ${username}');
+	});
 
 	db.close((err) => {
 		if (err) {
 			return console.error(err.message);
 		}
 		console.log('Closed the login databse connection');
-	})
+	});
 }
 
 
