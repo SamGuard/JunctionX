cf3759980f61a2d1f21e281c4ce261e996a756fb
SGuard27 <Guards27@hotmail.com>
2020-02-22 10:33:27 +0000

    Update main.js

--- Server/routes/main.js
+++ Server/routes/main.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
         var password = $("#password").val();
 
         $.ajax({
-            url: "register",
+            url: "auth",
             method: "POST",
             dataType: "json",
             crossDomain: true,
