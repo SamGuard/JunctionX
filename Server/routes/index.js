fe01526fb2112bf8536d8f43964981138fd78048
SGuard27 <Guards27@hotmail.com>
2020-02-22 00:03:09 +0000

    fujospa

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -8,10 +8,15 @@ router = express.Router();
 router.get("/", function (req, res, next){//When a get request is made on this directory on this server this function is called
 	
 	console.log("connection of index-home");
-	
+
 	
 	fs.readFile(process.cwd() + "/routes/index.html", "utf8", function(err, data){
 		res.send(data);//Send data back to user
+	});
+
+	router.post("/", function(req, res, next){
+		console.log(req);
+		res.send("f you jospa");
 	});
 	
 });
