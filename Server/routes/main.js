b22bc10ca219258898fee56e16ed6f3856c409dc
jcryer <jjcryer@mac.com>
2020-02-22 02:26:45 +0000

    Yay

--- Server/routes/main.js
+++ Server/routes/main.js
@@ -1,11 +1,29 @@
 
 $(document).ready(function() {
-    $("submit").click(function(){
-        var username = $("username").val();
-        var password = $("password").val();
+    $("#submit").click(function(){
+        var username = $("#username").val();
+        var password = $("#password").val();
+        
+        $.ajax({
+            url: "register",
+            method: "POST",
+            dataType: "json",
+            crossDomain: true,
+            contentType: "application/json; charset=utf-8",
+            data: {username: "fuck you sam"},
+            cache: false,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+                xhr.setRequestHeader("X-Mobile", "false");
+            },
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
 
-        $.post("login", {username: username, password: password}, function(result){
-            console.log(result);
+            }
         });
+        
+
     });
-}
+});
