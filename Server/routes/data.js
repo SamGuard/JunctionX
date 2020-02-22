53a1010b98f05b07cc020409804039d9f468d163
SGuard27 <Guards27@hotmail.com>
2020-02-22 15:56:44 +0000

    Update data.js

--- Server/routes/data.js
+++ Server/routes/data.js
@@ -4,7 +4,8 @@ router = express.Router();
 
 router.get("/", function(req,res,next){
 	//var body = req.body;
-	var body.type = "load";
+	var body = {};
+	body.type = "load";
 
 	//var body = JSON.parse('{"type":"trackNames","id": 1}');
 	if(body.type == "load"){
