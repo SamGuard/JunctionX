b9eeb2839f39a6438283d0cfafeaa9711dfb7065
Alfie Richards <alfierchrds@gmail.com>
2020-02-23 12:22:21 +0000

    jh

--- routes/main.js
+++ routes/main.js
@@ -334,6 +334,7 @@ function runGoalCallback(goalID, toggle) {
     }
 }
 
+<<<<<<< Updated upstream
 function showDiv(goToRegister) {
     if (goToRegister) {
         $("#loginInfo").hide();
@@ -345,6 +346,32 @@ function showDiv(goToRegister) {
         $("#loginInfo").show();
         $("#registerInfo").hide();
         $("#loginSectionID").css("height", "400px");
+=======
+    function drawChart(goalID, res) {
+        console.log(res);
+
+        var output = [];
+
+          for (var i=0;i<=res.x.length;i++) {
+             output[i] = [];
+          }
+        output[0][0] = 'Week';
+        output[0][1] = 'Days completed';
+
+        for (var i = 1; i <= res.x.length; i++) {
+            output[i][0] = res.x[i-1];
+            output[i][1] = res.y[i-1];
+        }
+
+        console.log(output);
+        var data = google.visualization.arrayToDataTable(output);
+
+        var options = {
+            title: 'Historical Evidence',
+            curveType: 'function',
+            legend: { position: 'bottom' }
+        };
+>>>>>>> Stashed changes
 
     }
 }
@@ -375,4 +402,3 @@ function setupAccordion() {
         }
       });
     }
-}
