d6b819a1ecfc7dfdf8a207e421c7105e2dce5dd9
SGuard27 <Guards27@hotmail.com>
2020-02-23 01:01:15 +0000

    Update data.js

--- routes/data.js
+++ routes/data.js
@@ -18,15 +18,25 @@ router.post("/", function(req,res,next){
 
 		out.tracks = tracks;
 
+
+		var goalInfo;
 		for(var i = 0; i < out.tracks.length; i++){
 			out.tracks[i].goals = dbHandler.getGoalsForTrack(out.tracks[i].track_id);
+			for(var j = 0; j < out.tracks[i].goals.length; j++){
+				goalInfo = dbHandler.getGoalScore(body.username, Date.now(), body.trackID, body.goalID);
+
+				out.track[i].goal.goal_score = goalInfo.goal_score;
+			}
 		}
+
 
 		res.json(out);
 
 	}else if(body.type == "compGoal"){
 		dbHandler.updateGoalScore(getUsername(req.headers["authorization"],new Date(),body.trackID,body.goalID))
-	
+		res.send("done");
+	}else if(body.type == "addGoal"){
+
 	}else if(body.type == "getGoalInfo"){
 		res.json(dbHandler.getGoalScore(body.username, Date.now(), body.trackID, body.goalID));
 	}else{
