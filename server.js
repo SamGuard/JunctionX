6f19f860ec5484d9feb4f50443bc054108cfce5a
SGuard27 <Guards27@hotmail.com>
2020-02-23 12:21:04 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const http = require('http');//Loading http package so i can use a http server
 const app = require("./app");//Getting app.js in the current folder which holds all the routes the user can take
-
-
+const dbHandler = require("./route/db")
 
 const port = process.env.PORT || 3000;//This means to use port 3000 unless process.env.PORT is set as this variable is set when deployed to heroku
 
@@ -14,5 +13,8 @@ console.log("listening on port " + port);
 
 
 setTimeout(function(){
-	console.log("update");
+	var users = dbHandler.getAllUsers();
+	for(var i = 0; i < users.length; i++){
+		db.updateUserScore(user[i].username, Date.now()-604800000);
+	}
 },100000);
